6615ff399368772b31b059562f02039a3c430255
amhmarat <36004882+amhmarat@users.noreply.github.com>
2018-09-14 20:07:50 +0300

    Версия 1

---
+++ src/scandir/DataFiles.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the  editor.
+ */
+package scandir;
+
+import java.io.File;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Date;
+//import java.io.Serializable;
+//import java.util.Comparator;
+
+/**
+ *
+ * @author Aymaletdinov MH
+ */
+public class DataFiles {
+    //private final DataListDir listWithAllFileNames;
+    private final File f;
+    private final Date dtFile;
+    private final BasicFileAttributes attrFile;
+    public DataFiles (File F, Date dtF, BasicFileAttributes attrF) {
+        this.f = F;    
+        this.dtFile = dtF;
+        this.attrFile = attrF;
+    }
+    public File getFile () {
+        return f;
+    }
+    public Date getFileDate () {
+        return dtFile;
+    }
+    public BasicFileAttributes getFileAttr () {
+        return attrFile;
+    }
+}
